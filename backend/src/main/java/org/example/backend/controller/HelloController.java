5b09745555020a583273bbf2e6fb11fdfe3df9a7
Roland-Hufnagel <roland.hufnagel@gmx.de>
2026-07-13 10:53:38 +0200

    add module frontend

---
+++ backend/src/main/java/org/example/backend/controller/HelloController.java
@@ -0,0 +1,12 @@
+package org.example.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet() {
+        return "Hello Session Planner";
+    }
+}
