bbf398d9e9270072ab10119610506bb5a9247241
Roland-Hufnagel <roland.hufnagel@gmx.de>
2026-07-13 11:58:14 +0200

    implement simple frontend that fetches the backend using axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
 
+    const [welcome, setWelcome] = useState("");
+    useEffect(() => {
+        axios.get("/api").then(res => setWelcome(res.data)).catch(e => setWelcome(e.message))
+    }, []);
     return (
-        <>
-
-        </>
+        <h1>
+            {welcome}
+        </h1>
     )
 }
 
