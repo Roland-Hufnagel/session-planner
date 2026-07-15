c1887a8459f58ebebc6a12669bc0ba3e217c7deb
Roland-Hufnagel <roland.hufnagel@gmx.de>
2026-07-15 15:28:20 +0200

    add Entity User

---
+++ backend/src/main/java/org/example/backend/model/User.java
@@ -0,0 +1,40 @@
+package org.example.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // Hibernate automatically creates table name 'github_name' from field 'githubName'
+    @Column(nullable = false, unique = true)
+    private String githubName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // Can be null -> no @Column constraints
+    private String avatarUrl;
+}
+
+
